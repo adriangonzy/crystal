1a7fe084053f3119c0aa902f8e3c2bf4656ee3bb
Adrian <adrian@meditect.com>
2021-11-07 21:41:39 +0100

    chore: add more shadow to card

--- front/src/components/Card/Card.tsx
+++ front/src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ export const Card: React.FunctionComponent<PropsWithChildren<CardProps>> = ({
     <div
       className="py-4 px-8 text-white bg-white bg-opacity-20 
                 rounded-lg border border-r-0 border-b-0 border-opacity-30 
-                shadow-2xl backdrop-filter backdrop-blur-sm"
+                shadow-5xl backdrop-filter backdrop-blur-sm"
     >
       {title && (
         <h2 className="mb-4 text-xl font-bold text-white uppercase">{title}</h2>
